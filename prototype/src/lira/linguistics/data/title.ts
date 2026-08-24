c434814e13f670a9475d6e7ceae0ead315f5ce70
ggilluk <53226170+ggilluk@users.noreply.github.com>
2026-08-24 06:07:03 +0200

    Remove misplaced HTML5 Title entity

--- prototype/src/lira/linguistics/data/title.ts
+++
@@ -1,9 +0,0 @@
-import type { LinguisticUnit } from "./linguistic_unit";
-
-/** HTML5 <title>. Its Text value is linguistic input and therefore
- * materialises as a LinguisticUnit. */
-export interface Title extends LinguisticUnit {}
-
-export function createTitle(init: Pick<Title, "text"> & Partial<Title>): Title {
-  return { ...init };
-}
