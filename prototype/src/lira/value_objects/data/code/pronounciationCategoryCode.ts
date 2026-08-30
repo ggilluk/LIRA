24b00ee7d0a66b7f1dc2792a4118abf0257dec42
ggilluk <53226170+ggilluk@users.noreply.github.com>
2026-08-30 21:00:20 +0200

    Make PronounciationCategoryCode CCTS compliant

--- prototype/src/lira/value_objects/data/code/pronounciationCategoryCode.ts
+++ prototype/src/lira/value_objects/data/code/pronounciationCategoryCode.ts
@@ -1,14 +1,39 @@
 import type { Code } from "../code";
 import { PronounciationCategoryCodelist } from "../enum/pronounciationCategoryCodelist";
 
-/** Pronunciation category Code using the major sections of the IPA chart. */
+/**
+ * UN/CEFACT CCTS Code. Type specialised for a pronunciation category.
+ *
+ * CCTS Code. Content is represented by `value`. The category code list is a
+ * LIRA code list aligned to the major sections of the official IPA chart, so
+ * no UNCL 3055 agency identifier is asserted.
+ */
 export class PronounciationCategoryCode implements Code {
   readonly value: PronounciationCategoryCodelist;
-  readonly listName = "International Phonetic Alphabet - Categories";
-  readonly listAgencyId = "ZZZ";
-  readonly listAgencyName = "International Phonetic Association";
 
-  constructor(value: PronounciationCategoryCodelist) {
+  readonly name?: string;
+  readonly languageId?: string;
+  readonly listId?: string;
+  readonly listAgencyId?: string;
+  readonly listAgencyName?: string;
+  readonly listName = "Pronounciation Category";
+  readonly listVersionId?: string;
+  readonly listUri?: string;
+  readonly listSchemeUri?: string;
+
+  constructor(
+    value: PronounciationCategoryCodelist,
+    name?: string,
+    languageId?: string,
+    listVersionId?: string,
+    listUri?: string,
+    listSchemeUri?: string,
+  ) {
     this.value = value;
+    this.name = name;
+    this.languageId = languageId;
+    this.listVersionId = listVersionId;
+    this.listUri = listUri;
+    this.listSchemeUri = listSchemeUri;
   }
 }
