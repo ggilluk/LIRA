5c5bed3885ec080aae622ce2c8047585b9e49d05
ggilluk <53226170+ggilluk@users.noreply.github.com>
2026-08-30 21:00:02 +0200

    Make pronunciation category codelist CCTS string codes

--- prototype/src/lira/value_objects/data/enum/pronounciationCategoryCodelist.ts
+++ prototype/src/lira/value_objects/data/enum/pronounciationCategoryCodelist.ts
@@ -1,10 +1,16 @@
-/** Major sections of the official International Phonetic Alphabet chart. */
+/**
+ * LIRA code values for the major sections of the official
+ * International Phonetic Alphabet chart.
+ *
+ * String values are used because UN/CEFACT CCTS Code. Content is a
+ * character string.
+ */
 export enum PronounciationCategoryCodelist {
-  CONSONANT_PULMONIC = 0,
-  CONSONANT_NON_PULMONIC = 1,
-  OTHER_SYMBOL = 2,
-  VOWEL = 3,
-  DIACRITIC = 4,
-  SUPRASEGMENTAL = 5,
-  TONE_AND_WORD_ACCENT = 6,
+  CONSONANT_PULMONIC = "CONSONANT_PULMONIC",
+  CONSONANT_NON_PULMONIC = "CONSONANT_NON_PULMONIC",
+  OTHER_SYMBOL = "OTHER_SYMBOL",
+  VOWEL = "VOWEL",
+  DIACRITIC = "DIACRITIC",
+  SUPRASEGMENTAL = "SUPRASEGMENTAL",
+  TONE_AND_WORD_ACCENT = "TONE_AND_WORD_ACCENT",
 }
