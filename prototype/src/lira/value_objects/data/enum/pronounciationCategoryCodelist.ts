402539a3336695d6a29a8cfaab735087c2a934a3
ggilluk <53226170+ggilluk@users.noreply.github.com>
2026-08-30 20:40:46 +0200

    Rename pronunciation category enum to Codelist

---
+++ prototype/src/lira/value_objects/data/enum/pronounciationCategoryCodelist.ts
@@ -0,0 +1,10 @@
+/** Major sections of the official International Phonetic Alphabet chart. */
+export enum IPACategoryCodelist {
+  CONSONANT_PULMONIC = 0,
+  CONSONANT_NON_PULMONIC = 1,
+  OTHER_SYMBOL = 2,
+  VOWEL = 3,
+  DIACRITIC = 4,
+  SUPRASEGMENTAL = 5,
+  TONE_AND_WORD_ACCENT = 6,
+}
