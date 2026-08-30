f19967bf4d79d309d2d1a89d1aa01727520c8aa4
ggilluk <53226170+ggilluk@users.noreply.github.com>
2026-08-30 20:45:22 +0200

    Use Pronounciation codelist enum names

--- prototype/src/lira/value_objects/data/code/pronounciationCode.ts
+++ prototype/src/lira/value_objects/data/code/pronounciationCode.ts
@@ -1,11 +1,11 @@
 import type { Code } from "../code";
-import { IPACodelist } from "../enum/pronounciationCodelist";
-import { IPACategoryCodelist } from "../enum/pronounciationCategoryCodelist";
+import { PronounciationCodelist } from "../enum/pronounciationCodelist";
+import { PronounciationCategoryCodelist } from "../enum/pronounciationCategoryCodelist";
 
 /** Pronunciation Code using the International Phonetic Alphabet. */
 export class PronounciationCode implements Code {
-  readonly value: IPACodelist;
-  readonly category: IPACategoryCodelist;
+  readonly value: PronounciationCodelist;
+  readonly category: PronounciationCategoryCodelist;
   readonly ipaNumber?: string;
   readonly unicodeCodePoints: string[];
   readonly unicodeNames: string[];
@@ -13,7 +13,7 @@ export class PronounciationCode implements Code {
   readonly listAgencyId = "ZZZ";
   readonly listAgencyName = "International Phonetic Association";
 
-  constructor(value: IPACodelist, category: IPACategoryCodelist, unicodeCodePoints: string[] = [], unicodeNames: string[] = [], ipaNumber?: string) {
+  constructor(value: PronounciationCodelist, category: PronounciationCategoryCodelist, unicodeCodePoints: string[] = [], unicodeNames: string[] = [], ipaNumber?: string) {
     this.value = value;
     this.category = category;
     this.unicodeCodePoints = unicodeCodePoints;
