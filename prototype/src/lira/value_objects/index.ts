e5d670afef28b5a2ac13a8ed02e4134a76dbad4c
ggilluk <53226170+ggilluk@users.noreply.github.com>
2026-08-30 20:39:24 +0200

    Update pronunciation code exports

--- prototype/src/lira/value_objects/index.ts
+++ prototype/src/lira/value_objects/index.ts
@@ -13,12 +13,8 @@ export { ValueObjectAgent } from "./agents";
 export { ValueObjectsLayer } from "./data/layer";
 export { type Code, code } from "./data/code";
 export { PronounciationCode } from "./data/code/pronounciationCode";
-export { PronounciationCategoryCode } from "./data/code/pronounciationCategoryCode";
-export {
-  IPA_CODE_LIST,
-  type IPACode,
-  ipaCode,
-} from "./data/ipa_code_list";
+export { IPACode } from "./data/enum/pronounciationCode";
+export { IPACategoryCode } from "./data/enum/pronounciationCategoryCode";
 export { type Identifier, identifier } from "./data/identifier";
 export { type Number_, number } from "./data/number";
 export { type Text, text } from "./data/text";
